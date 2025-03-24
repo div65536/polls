4bfcb4b089c1118f4c0da9d587c813c610950a1a
Divyansh <divyanshyadav@thoughtwin.com>
2025-03-24 15:43:51 +0530

    added class based view for vote

--- poll/views.py
+++ poll/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
+from django.views import generic,View
 
 from .models import Choice, Question
 
@@ -18,6 +18,7 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    context_object_name = "ques"
 
 
 class ResultsView(generic.DetailView):
@@ -26,4 +27,30 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-    pass
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'ques':question,
+            'error_message':"You didn't select a choice"
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args = (question.id,)))
+    
+class VoteView(View):
+    def post(self,request,question_id):
+        question = get_object_or_404(Question,pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except(KeyError,Choice.DoesNotExist):
+            return render(request,'polls/detail.html',{
+                "ques":question,
+                "error_message":"You did not select a choice"
+            })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
